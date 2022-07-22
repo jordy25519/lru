4b2b3eab45aa8b5459ea9e193b660726c61957ca
holygits <beauchjord@gmail.com>
2022-07-22 13:47:45 +1200

    Use a dq

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 // use std::collections::HashMap;
 use fnv::FnvHashMap as HashMap;
-use std::hash::Hash;
+use std::{hash::Hash, collections::VecDeque};
 
 trait LRUCache<K, V> {
     fn initialize(max: u16) -> Self;
@@ -9,9 +9,13 @@ trait LRUCache<K, V> {
 }
 
 pub struct Cache<K: Eq + Hash, V> {
+    /// generation index
     g: u32,
+    /// max capacity of the cache
     max: u16,
+    /// cached items
     items: HashMap<K, (V, u32)>,
+    recency_buckets: VecDeque<(u32, K)>,
 }
 
 impl<'a, K: Eq + Hash + Clone, V: Copy> LRUCache<K, V> for Cache<K, V> {
@@ -23,33 +27,46 @@ impl<'a, K: Eq + Hash + Clone, V: Copy> LRUCache<K, V> for Cache<K, V> {
                 max as usize,
                 Default::default(),
             ),
+            recency_buckets: VecDeque::with_capacity(max as usize),
         }
     }
     fn get(&mut self, k: K) -> Option<V> {
         let entry = self.items.get_mut(&k);
         if let Some(entry) = entry {
-            entry.1 = self.g + 1;
-            self.g += 1;
+            let recency = self.g + 1;
+
+            // update recency info for the key
+            // keeping it sorted allows O(1) expiry later
+            if let Ok(idx) = self.recency_buckets.binary_search_by(|(g, _k)| g.cmp(&entry.1)) {
+                if idx == 0 {
+                    let _ = self.recency_buckets.pop_front();
+                } else {
+                    // worst case is (O(N / 2)) since it shift on the shorter side
+                    self.recency_buckets.remove(idx);
+                }
+            }
+            self.recency_buckets.push_back((recency, k));
+
+            entry.1 = recency;
+            self.g = recency;
+
             Some(entry.0)
         } else {
             None
         }
     }
     fn set(&mut self, k: K, v: V) {
-        let mut oldest_g = u32::max_value();
-        let mut evict: Option<K> = None;
-        if self.items.len() + 1 > self.max as usize {
-            for (k, v) in self.items.iter() {
-                if v.1 < oldest_g {
-                    evict = Some(k.clone());
-                    oldest_g = v.1;
-                }
-            }
-        }
-        if let Some(ref evict) = evict {
-            self.items.remove(evict);
+        if self.items.contains_key(&k) {
+            return
         }
 
+        // evict
+        if self.items.len() + 1 > self.max as usize {
+            let evict = self.recency_buckets.pop_front().unwrap();
+            self.items.remove(&evict.1);
+        }
+
+        self.recency_buckets.push_back((self.g, k.clone()));
         self.items.insert(k, (v, self.g));
     }
 }
@@ -72,12 +89,46 @@ mod tests {
     fn expires_lru() {
         let mut cache = Cache::initialize(3);
         cache.set(1, "a");
-        cache.set(2, "b");
-        cache.set(3, "c");
         let _ = cache.get(1);
+        println!("{:?}", cache.items);
+        println!("{:?}", cache.recency_buckets);
+        cache.set(2, "b");
         let _ = cache.get(2);
+        println!("{:?}", cache.items);
+        println!("{:?}", cache.recency_buckets);
+        cache.set(3, "c");
+        println!("{:?}", cache.items);
+        println!("{:?}", cache.recency_buckets);
+        let _ = cache.get(1);
+        println!("{:?}", cache.items);
+        println!("{:?}", cache.recency_buckets);
+        let _ = cache.get(2);
+        println!("{:?}", cache.items);
+        println!("{:?}", cache.recency_buckets);
         cache.set(4, "d");
+        println!("{:?}", cache.items);
+        println!("{:?}", cache.recency_buckets);
         assert_eq!(cache.items.len(), cache.max as usize);
         assert!(cache.get(3).is_none());
     }
+
+    // #[test]
+    // fn expires_lru_bench() {
+    //     let count = 100_000;
+    //     let mut cache = Cache::initialize(count);
+
+    //     for c in 0..count {
+    //         cache.set(1, "a");
+    //         cache.set(2, "b");
+    //         cache.set(3, "c");
+    //         cache.set(4, "d");
+    //     }
+    // }
 }
+
+// hashmap cache, recency update on get O(1), set: hashmap iterate all keys to expire oldest O(N)
+
+// hashmap cache + recency queue, recency update on get O(log N), set: lookup to expire O(1)
+    // figure out best swap_remove so we don't cause left shift of vec
+
+// hashmap cache + splay tree, recency update on get O(log N), set: lookup to expire O(1)
